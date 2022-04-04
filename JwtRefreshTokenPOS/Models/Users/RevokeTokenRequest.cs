805c604a99ccb40dfedba15b14d7ac20c8a4d92b
Azeem-Dev <azeemsharif5822@gmail.com>
2022-04-04 12:28:12 +0500

    complete auth done with JWT

---
+++ JwtRefreshTokenPOS/Models/Users/RevokeTokenRequest.cs
@@ -0,0 +1,7 @@
+﻿namespace JwtRefreshTokenPOS.Models.Users
+{
+    public class RevokeTokenRequest
+    {
+        public string Token { get; set; }
+    }
+}
---
+++ JwtRefreshTokenPOS/Services/IUserService.cs
@@ -0,0 +1,14 @@
+﻿using JwtRefreshTokenPOS.Entities;
+using JwtRefreshTokenPOS.Models.Users;
+
+namespace JwtRefreshTokenPOS.Services
+{
+    public interface IUserService
+    {
+        AuthenticateResponse Authenticate(AuthenticateRequest model, string ipAddress);
+        AuthenticateResponse RefreshToken(string token, string ipAddress);
+        void RevokeToken(string token, string ipAddress);
+        IEnumerable<User> GetAll();
+        User GetById(int id);
+    }
+}
